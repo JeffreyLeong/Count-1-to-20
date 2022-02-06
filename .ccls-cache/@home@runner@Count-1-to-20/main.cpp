aa20332f78429caaad3fa116c522c7eb8ead320d
Jeffrey Leong <62955867+JeffreyLeong@users.noreply.github.com>
2022-02-06 00:50:45 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Count-1-to-20/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Write a complete C++ program that outputs the numbers 1 to 20, one per line. The program does nothing else.
+
+int main(){
+  int count = 0;
+  while (count <20){
+    count++;
+    cout << count << endl;
+  }
+  
+  return 0;
+}
